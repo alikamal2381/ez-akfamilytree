d8c4216fd2aa0b5c147f015d135bc7f5d09ae85f
AliKamal <alikamal2381@gmail.com>
2021-04-11 18:18:11 +0500

    test2.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+//import Test from "./components/Test";
 
 class App extends Component {
   render() {
@@ -10,6 +12,8 @@ class App extends Component {
         <Header />
         <Main />
         <Footer />
+
+        {/* <Test /> */}
       </>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,126 +1,70 @@
-import * as React from "react";
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+//import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import MyPic from "../images/root-img.png";
 import PostData from "../data/post.json";
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-    // width: "100%",
-    // margin: "auto",
-    alignItems: "center",
-  },
-  avatarimg: {
-    height: "250px",
-    //paddingTop: "5em",
-    borderRadius: "50%",
-    alignItems: "center",
-    alignContent: "center",
-  },
-  landinggrid: {
-    background: "#fc4a1a",
-    position: "fixed",
-    left: "0",
-    width: "100%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-function Main() {
-  const classes = useStyles();
-
-  console.log(PostData);
-  return (
-    <>
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <img
-            src={MyPic}
-            alt="avatar"
-            className={classes.avatarimg}
-            align="center"
-          />
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
+class Main extends Component {
+  render() {
+    //const data = PostData.JSON
+    console.log(PostData);
+    return (
+      <>
+        <main>
+          {/* Hero unit  landing-grid  heroContent  */}
+          <div className="heroContent">
+            <img
+              src={MyPic}
+              alt="avatar"
+              className="avatar-img"
               align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Dr. Mehfooz Ullah
-            </Typography>
+            />
+            <Container maxWidth="sm">
+              <h1>Dr. Mehfooz Ullah</h1>
+              <h3>Great Grand Father</h3>
+              {/*               <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="textPrimary"
+              >
+                Dr. Mehfooz Ullah
+              </Typography>
 
-            <Typography
-              component="p"
-              variant="h6"
-              align="center"
-              color="textPrimary"
-              gutterBottom
-            >
-              Great Grand Father
-            </Typography>
+              <Typography
+                component="p"
+                variant="h6"
+                align="center"
+                color="textPrimary"
+              >
+                Great Grand Father
+              </Typography> */}
+            </Container>
 
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
-            </Typography>
-          </Container>
-        </div>
-        <Container className={classes.landinggrid} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            <div alignContent="center" alignItems="center">
-              Content Main
-              {PostData.map((postDetail, index) => {
-                return <h1 key={index}>{postDetail.name}</h1>;
-              })}
-            </div>
-          </Grid>
-        </Container>
-      </main>
-    </>
-  );
+            <Container maxWidth="sm">
+              {/* End hero unit */}
+              <Grid spacing={4}>
+                <div container>
+                  {PostData.familyTree.map((postDetail, index) => {
+                    return (
+                      <div key={index}>
+                        <h5 key={index}>{postDetail.name}</h5>
+                      </div>
+                    );
+                  })}
+
+                  <h3 style={{ color: "red" }}>abcddsjdksjds dksjdksdjk</h3>
+
+                  {JSON.stringify(PostData.familyTree, null, 2)}
+                </div>
+              </Grid>
+            </Container>
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default Main;
