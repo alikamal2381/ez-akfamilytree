8c8785935e1d1ea50a4272e652b3ff6c46335d2e
alikamal2381 <alikamal2381@gmail.com>
2022-10-09 21:26:59 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,6 @@ class App extends Component {
         <Header />
         <Main />
         <Footer />
-
-        <p>othererer kldksldks kldskd sdkks d</p>
-        <h3>I love you....</h3>
-        <h1>ddddsdsd dsd</h1>
-        <h3>New changes dddd123</h3>
       </>
     );
   }
